f324d7a1d0d1acece5df1533a5520f5d6fccf8c2
Rafaella <carmo.rafaella@yahoo.com.br>
2022-01-24 14:59:54 -0300

    style: styling form and list

--- src/components/CardNota/CardNota.jsx
+++ src/components/CardNota/CardNota.jsx
@@ -1,11 +1,15 @@
 import React, { Component } from "react";
 
+import "./CardNota.css"
+
 class CardNota extends Component {
   render() {
     return (
-      <section>
-        <header>Titulo</header>
-        <p>Escreva sua nota</p>
+      <section className="card-nota">
+        <header className="card-nota_cabecalho">
+          <h3 className="card-nota_titulo">Título</h3>
+        </header>
+        <p className="card-nota_texto">Escreva sua nota</p>
       </section>
     );
   }
--- src/components/ListaDeNotas/ListaDeNotas.jsx
+++ src/components/ListaDeNotas/ListaDeNotas.jsx
@@ -1,14 +1,15 @@
 import React, { Component } from "react";
-import CardNota from "./CardNota";
+import CardNota from "../CardNota/CardNota";
+
+import "./ListaDeNotas.css"
 
 class ListaDeNotas extends Component {
   render() {
     return (
-      <ul>
-        {Array.of("Estudos", "Trabalho", "Lazer").map((categoria, index) => {
+      <ul className="lista-notas">
+        {Array.of("Trabalho", "Trabalho", "Estudos").map((categoria, index) => {
           return (
-            <li key={index}>
-              {categoria}
+            <li className="lista-notas_item" key={index}>
               <CardNota />
             </li>
           );
